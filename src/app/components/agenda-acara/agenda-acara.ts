8b0df3dddb58119139febbeacd0d35f69a4c859e
latifaweb <emailuntukkomputerkantor@gmail.com>
2025-07-05 17:13:26 +0700

    u jejak cerita

--- src/app/components/agenda-acara/agenda-acara.ts
+++ src/app/components/agenda-acara/agenda-acara.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+interface Story {
+  nama: string;
+  highlight: string;
+  cerita: string;
+  foto: string;
+}
 
 @Component({
   selector: 'app-agenda-acara',
-  imports: [],
+  imports: [CommonModule, HttpClientModule, RouterLink],
   templateUrl: './agenda-acara.html',
   styleUrl: './agenda-acara.css'
 })
-export class AgendaAcara {
+export class AgendaAcara implements OnInit {
+  stories: Story[] = [];
+  loading = true;
+  error: string | null = null;
+  expandedStories: boolean[] = [];
+  
+  private apiUrl = 'https://script.google.com/macros/s/AKfycbyvEg0vJv9L7jfrcy1DVJrSNBPxgMrnDXDiilJ1VouqAc2IxTWtdhTH3waLQoPxWr8_/exec';
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadStories();
+  }
+
+  loadStories() {
+    this.loading = true;
+    this.error = null;
+    
+    this.http.get<Story[]>(this.apiUrl).subscribe({
+      next: (data) => {
+        this.stories = data;
+        this.expandedStories = new Array(data.length).fill(false);
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = 'Gagal memuat cerita. Silakan coba lagi.';
+        this.loading = false;
+        console.error('Error loading stories:', err);
+      }
+    });
+  }
+
+  toggleExpanded(index: number) {
+    this.expandedStories[index] = !this.expandedStories[index];
+  }
+
+  onImageError(event: any) {
+    event.target.src = 'https://via.placeholder.com/600x400/374151/9CA3AF?text=Gambar+Tidak+Tersedia';
+  }
 }
